dc2b78f20cf2025f32c197d4cd55ba7b075c66d2
Sai usha sri Kashi <saikashi@publicisgroupe.net>
2022-11-29 20:21:00 +0530

    file added

---
+++ src/main/java/practice/HelloFirstNameLastName.java
@@ -0,0 +1,18 @@
+package practice;
+
+import java.util.Scanner;
+
+public class HelloFirstNameLastName {
+
+	public static void main(String[] args) {
+	
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter First Name : ");
+		String firstName=sc.nextLine();
+		System.out.println("Enter last name : ");
+		String lastName=sc.nextLine();
+		System.out.println("Hello "+firstName+" "+lastName);
+
+	}
+
+}
